22569d4c711bf51438516c1c9855de679a4fc89e
henriquepaduelim <80224120+henriquepaduelim@users.noreply.github.com>
2025-09-25 12:35:07 -0400

    Frontend starting

---
+++ Crud/frontend/src/main/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+
+import Nav from '../components/template/Nav'
+import Logo from '../components/template/logo'
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
+
+export default props =>
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main />
+        <Footer />
+    </div>
