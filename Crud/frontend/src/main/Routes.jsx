b38794b7bcc86de53892b1cc02b110cf2d194a76
henriquepaduelim <80224120+henriquepaduelim@users.noreply.github.com>
2025-09-28 09:23:26 -0400

    Responsiveness_fixed

--- Crud/frontend/src/main/Routes.jsx
+++ Crud/frontend/src/main/Routes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom"; // <- dom!
+import { Switch, Route, Redirect } from "react-router-dom"; 
 
 import Home from "../components/template/home/Home";
-import UserCrud from "../components/template/user/UserCrud"; // ajuste o path se necessário
+import UserCrud from "../components/template/user/UserCrud"; 
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />      {/* component (minúsculo) */}
+      <Route exact path="/" component={Home} />      
       <Route path="/users" component={UserCrud} />
       <Redirect from="*" to="/" />
     </Switch>
